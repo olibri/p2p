a894638e3e2b02869755870912d3341fc1cca2fc
Volodya <volodymyr.mykhailovych@intenselab.eu>
2024-09-15 13:24:10 +0300

    WIP

--- src/app/online-market/express-market/express-market.component.ts
+++ src/app/online-market/express-market/express-market.component.ts
@@ -1,12 +1,16 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-express-market',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+
+  ],
   templateUrl: './express-market.component.html',
   styleUrl: './express-market.component.css'
 })
 export class ExpressMarketComponent {
-
+  activeTab: string = 'buy'; // Default active tab
 }
